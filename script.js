0025882f3dceae9d1f45e30d56bf0cf90ce15329
Nazar0929 <166243366+Nazar0929@users.noreply.github.com>
2024-09-20 14:34:23 +0300

    hw25

---
+++ script.js
@@ -0,0 +1,31 @@
+const squaree = document.querySelector('.squaree');
+
+squaree.addEventListener('mouseenter', () => {
+    document.body.style.background = '#ff7e5f';
+});
+
+squaree.addEventListener('mouseleave', () => {
+    document.body.style.background = ''; 
+});
+
+
+const squareee = document.querySelector('.squareee');
+
+squareee.addEventListener('mouseenter', () => {
+    document.body.style.background = '#003dae'; 
+});
+
+squareee.addEventListener('mouseleave', () => {
+    document.body.style.background = ''; 
+});
+
+
+const sg = document.querySelector('.sg');
+
+sg.addEventListener('mouseenter', () => {
+    document.body.style.background = '#0d680d'; 
+});
+
+sg.addEventListener('mouseleave', () => {
+    document.body.style.background = ''; 
+});
